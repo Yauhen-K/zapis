3fc292603441b3ae9a456bc64d94527bc049492a
Yauhen <canystra@gmail.com>
2021-07-28 12:08:19 +0300

    Для точного выолнения задания добавил второе поле.Как вариант.

---
+++ src/com/file/ya/задания/выполненные/изменениеРегистра/IncreaseLetters.java
@@ -0,0 +1,17 @@
+package com.file.ya.задания.выполненные.изменениеРегистра;
+
+public class IncreaseLetters {
+    public String text;
+    public String name;
+
+    public String ApLetters(String txt) {
+        return txt.toUpperCase();
+
+    }
+
+    public String sum(String ap) {
+        return  text += ap ;
+    }
+
+
+}
