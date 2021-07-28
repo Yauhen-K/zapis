9f0d7b45cc174075dc74ffffccc69dbb677122d1
Yauhen <canystra@gmail.com>
2021-07-28 13:23:21 +0300

    Для точного выолнения задания добавил второе поле. Как вариант можно, наверно, передать слово "привет" в качестве аргумента из мейна, но там есть вопрос.

--- src/com/file/ya/задания/выполненные/изменениеРегистра/IncreaseLetters.java
+++ src/com/file/ya/задания/выполненные/изменениеРегистра/IncreaseLetters.java
@@ -1,16 +1,14 @@
 package com.file.ya.задания.выполненные.изменениеРегистра;
 
 public class IncreaseLetters {
-    public String text;
-    public String name;
 
-    public String ApLetters(String txt) {
+    public String apLetters(String txt) {
         return txt.toUpperCase();
 
     }
 
-    public String sum(String ap) {
-        return  text += ap ;
+    public String sum(String txt ) {
+        return ("привет  " + txt);
     }
 
 
