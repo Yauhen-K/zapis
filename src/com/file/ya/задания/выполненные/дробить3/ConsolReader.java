3d691f3267086c8cc8cfccb46bbf8284f18dce29
Yauhen <canystra@gmail.com>
2021-07-28 12:26:07 +0300

    Для точного выолнения задания добавил второе поле.Как вариант можно, наверно, передать слово привет в качестве аргумента из мейна, но там есть вопрос.

--- src/com/file/ya/задания/выполненные/дробить3/ConsolReader.java
+++ src/com/file/ya/задания/выполненные/дробить3/ConsolReader.java
@@ -1,4 +1,4 @@
-package com.file.ya.задания;
+package com.file.ya.задания.выполненные.дробить3;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -6,25 +6,22 @@ import java.io.InputStreamReader;
 import java.io.Reader;
 import java.util.ArrayList;
 
-public class ВводВыводЭрейлистКонсоль {
-    public static void main(String[] args) throws IOException {
+public class ConsolReader {
+    public void read() throws IOException {
         Reader r = new InputStreamReader(System.in);
         BufferedReader reader = new BufferedReader(r);
         System.out.println(" введите данные в косоль");
-        ArrayList<String> list =new ArrayList<String>();
+        ArrayList<String> list = new ArrayList<String>();
         for (int i = 0; i < 3; i++) {
             String s = reader.readLine();
             list.add(s);
 
             for (int j = 0; j < list.size(); j++) {
-                int u = list.size()-j-1;
+                int u = list.size() - j - 1;
                 System.out.println(list.get(u));
 
             }
         }
-
-
     }
-
-
 }
+
