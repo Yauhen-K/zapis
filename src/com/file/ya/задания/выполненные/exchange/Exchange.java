8c087335a3401b6de83e896aaf3010d0ac4dc6b0
Yauhen <canystra@gmail.com>
2021-08-18 10:27:53 +0300

    переделал

--- src/com/file/ya/задания/выполненные/exchange/Exchange.java
+++
@@ -1,9 +0,0 @@
-package com.file.ya.задания.выполненные.exchange;
-
-public class Exchange {
-    public static void main(String[] args) {
-        CurrencyExchange exchange =new CurrencyExchange();
-        double curs = exchange.currencyExchange( 156);
-        System.out.println(curs);
-    }
-}
